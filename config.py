03657cce62c638fcca2a89e80efaa039efc6e434
Mark Steve Samson <contact@marksteve.me>
2012-03-29 05:31:36 +0800

    Forgot to add config module.

---
+++ config.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+
+DEV_SERVER_HOST = 'localhost'
+DEV_SERVER_PORT = 34897
+ENVIRON_FILE = '/home/dotcloud/environment.json'
+
+
+if os.path.exists(ENVIRON_FILE):
+  debug = False
+  with open(ENVIRON_FILE) as f:
+    env = json.load(f)
+else:
+  debug = True
+  env = os.environ
+
+mod_path = os.path.dirname(__file__)
+root_url = env.get('PROD_SERVER_URL', 'http://%s:%s' % (DEV_SERVER_HOST, DEV_SERVER_PORT)).rstrip('/')
+# I'm too lazy to fix my nginx to work with websockets so let's connect
+# directly to the gunicorn server instead
+
+# Gunicorn config
+bind = '%s:%s' % (DEV_SERVER_HOST, DEV_SERVER_PORT)
+worker_class = 'ytpl.GeventWebSocketWorker'
